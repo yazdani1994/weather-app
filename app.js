5af4cbe179051a859590661384ecbe635e55272f
Pinocchio <tyazdani7@gmail.com>
2022-08-07 23:21:42 +0430

    new edit added

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ let apiData = {
 
 function fetchData() {
   let cityValue = inputElem.value;
-
   fetch(`${apiData.url}${cityValue}&&appid=${apiData.key}`).then((res) =>
     res.json().then((data) => {
       console.log(data);
